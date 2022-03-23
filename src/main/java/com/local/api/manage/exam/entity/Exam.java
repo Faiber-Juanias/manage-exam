e331197ed005881553ed1525eac1b5aeb04e4be7
Faiber Juanias <faiberjuanias001@gmail.com>
2022-03-23 06:51:01 -0500

    Se agrega el modelo para los exámenes

---
+++ src/main/java/com/local/api/manage/exam/entity/Exam.java
@@ -0,0 +1,43 @@
+package com.local.api.manage.exam.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "examen")
+public class Exam {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_examen")
+	private Integer idExam;
+	@Column(name = "nombre")
+	private String name;
+	
+	public Exam(String name) {
+		this.name = name;
+	}
+	
+	public Integer getIdExam() {
+		return idExam;
+	}
+	public void setIdExam(Integer idExam) {
+		this.idExam = idExam;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Exam [idExam=" + idExam + ", name=" + name + "]";
+	}
+	
+}
