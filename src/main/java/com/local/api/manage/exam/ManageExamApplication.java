5bf2f22f336372822cf7ed3b3444a2d9565425e0
Faiber Juanias <faiberjuanias001@gmail.com>
2022-03-23 01:35:35 -0500

    Initial commit

---
+++ src/main/java/com/local/api/manage/exam/ManageExamApplication.java
@@ -0,0 +1,13 @@
+package com.local.api.manage.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageExamApplication.class, args);
+	}
+
+}
